483025950283b03e8f7a2247e09b0fbdf9bdde2a
VladanStar <vladancupric@gmail.com>
2023-04-19 11:45:07 +0200

    Add

--- Task01/js/main.js
+++ Task01/js/main.js
@@ -7,25 +7,27 @@
 // console.warn("Error Warn")
 
 function inflationCalculator() {
-  let stopaInflacije = document.querySelector("#inflationRate").value;
-  console.log("stopaInflacije");
-  let money = document.querySelector("#money").value;
-
-  let sabiranje = parseInt(stopaInflacije) + parseInt(money);
-  console.log(sabiranje);
-
-  let years = document.querySelector('#years').value;
-  years = parseFloat(years)
-
-  let worth = money + (money + (stopaInflacije/100));
-  console.log(worth);
-  for(let i = 0; i<years;i++){
-    worth += worth*(stopaInflacije/100)
+    let stopaInflacije = document.querySelector("#inflationRate").value;
+    console.log(stopaInflacije);
+    let money = document.querySelector("#money").value;
+  
+    let sabiranje = parseInt(stopaInflacije) + parseInt(money);
+    console.log(sabiranje);
+  
+    let years = document.querySelector('#years').value;
+    years = parseFloat(years);
+  
+    let worth = parseFloat(money);
+    console.log(worth);
+    for(let i = 0; i<years;i++){
+      worth += worth*(stopaInflacije/100);
+    }
+    worth = parseFloat(worth.toFixed(2));
+    console.log(worth);
+  
+    let newElement = document.createElement('div');
+    newElement.className = 'new-value';
+    newElement.innerText = `Današnjih ${money} evra vredi isto kao i ${worth} evra za ${years} godina.`;
+    document.querySelector('.container').appendChild(newElement);
   }
-  console.log(worth)
-
-  let newElement = document.createElement('div');
-  newElement.className = 'new-value';
-  newElement.innerText = `Danasnjih ${money}E vedi isto kao i ${worth} za ${years} godina `
-document.querySelector('container').appendChild(newElement)
-}
+  
