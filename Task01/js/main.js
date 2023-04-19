0d44ecfae07ea12cc8d4f2b0c51ba757d29aaab1
VladanStar <vladancupric@gmail.com>
2023-04-19 10:58:30 +0200

    Add

--- Task01/js/main.js
+++ Task01/js/main.js
@@ -1,7 +1,11 @@
-window.alert("Pozdrav");
-document.write("Pozdrav");
-console.l
+// window.alert("Pozdrav");
+// document.write("Pozdrav");
+// console.l
 
-or("Vladan Cupric")
-console.error('Error');
-console.warn("Error Warn")
+// or("Vladan Cupric")
+// console.error('Error');
+// console.warn("Error Warn")
+
+function inflationCalculator() {
+  console.log("Radi");
+}
