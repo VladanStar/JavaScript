d9c17744a87d8b917ea37e029f6a3a4b3c80f20a
VladanStar <vladancupric@gmail.com>
2023-04-27 21:22:34 +0200

    Add

--- Task02/js/main.js
+++ Task02/js/main.js
@@ -1,16 +1,16 @@
-<<<<<<< HEAD
-function pokreniMe (){
-    alert("Fajlje ucitan")
-=======
-function pokreniMe(dugme){
-let nazivDugmeta = dugme.innerText;
-// let klasaDugmeta = dugme.classList.value;
-let klasaDugmeta = dugme.className;
-    let message = document.querySelector('#kliknuto_dugme')
-    message.innerText = "Pozdrav";
-console.log(klasaDugmeta)
-    console.log(this)
+// <<<<<<< HEAD
+// function pokreniMe (){
+//     alert("Fajlje ucitan")
+// =======
+// function pokreniMe(dugme){
+// let nazivDugmeta = dugme.innerText;
+// // let klasaDugmeta = dugme.classList.value;
+// let klasaDugmeta = dugme.className;
+//     let message = document.querySelector('#kliknuto_dugme')
+//     message.innerText = "Pozdrav";
+// console.log(klasaDugmeta)
+//     console.log(this)
 
-    // this je prenela celu funkciju
->>>>>>> 19298a9e614eb6c4445df30fa85dcd1c1e028130
-}
+//     // this je prenela celu funkciju
+// >>>>>>> 19298a9e614eb6c4445df30fa85dcd1c1e028130
+//}
