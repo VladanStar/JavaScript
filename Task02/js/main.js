bdb8f5c63d9e33985bb0482226ef47856f9e3873
VladanStar <vladancupric@gmail.com>
2023-04-27 11:31:19 +0200

    Add

--- Task02/js/main.js
+++ Task02/js/main.js
@@ -1,4 +1,9 @@
 function pokreniMe(dugme){
+let nazivDugmeta = dugme.innerText;
+let klasaDugmeta = dugme.classList;
+    let message = document.querySelector('#kliknuto_dugme')
+    message.innerText = "Pozdrav";
+console.log(klasaDugmeta)
     console.log(this)
 
     // this je prenela celu funkciju
